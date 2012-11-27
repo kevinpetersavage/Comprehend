c38a1b336e60390f0cccd896725938ec5de1b1ca
Kevin <kevinpetersavage@github.com>
2012-11-27 22:16:47 +0000

    more merging

--- src/org/comprehend/Comprehension.java
+++ src/org/comprehend/Comprehension.java
@@ -1,34 +1,94 @@
 package org.comprehend;
 
+import java.math.BigInteger;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.HashSet;
 import java.util.List;
+import java.util.Queue;
 import java.util.Set;
+import java.util.concurrent.ConcurrentLinkedQueue;
 
 public class Comprehension {
+	static public <T> Set<T> comprehendInParallel(Parameter<T> function, @SuppressWarnings("rawtypes") ParameterSetter... params) {
+		int availableProcessors = Runtime.getRuntime().availableProcessors();
+		return comprehendInParallel(availableProcessors == 1 ? 1 : availableProcessors-1, function, params);
+	}
+	
 	static public <T> Set<T> comprehend(Parameter<T> function, @SuppressWarnings("rawtypes") ParameterSetter... params) {
-		List<Integer> sizes = sizes(params);
-		long combinations = combinations(sizes);
+		return comprehendInParallel(1, function, params);
+	}
 
-		Set<T> results = new HashSet<T>();
-		for (long i = 0; i<combinations; i++){
-			long combinationsSoFar = 1;
-			for (int j = 0; j < params.length; j++) {
-				long size = sizes.get(j);
-				params[j].setParameter((i/combinationsSoFar)%size);
-				combinationsSoFar *= size;
-			}
-			results.add(function.evaluate());
+	static public <T> Set<T> comprehendInParallel(final int numberOfThreads, final Parameter<T> function, @SuppressWarnings("rawtypes") final ParameterSetter... params) {
+		final List<Integer> sizes = sizes(params);
+		final long combinations = combinations(sizes);
+
+		final Set<T> results = Collections.synchronizedSet(new HashSet<T>());
+		
+		List<Thread> threads = new ArrayList<Thread>();
+		final Queue<Long> queue = new ConcurrentLinkedQueue<Long>();
+		for (int i = 0; i < numberOfThreads; i++) {
+			sendMessageIfValid(queue, i, combinations);
+			threads.add(new Thread(){
+				@Override
+				public void run() {
+					Long i = null;
+					while ((i = queue.poll()) != null){
+						sendMessageIfValid(queue, i + numberOfThreads, combinations);
+						
+						long combinationsSoFar = 1;
+						for (int j = 0; j < params.length; j++) {
+							long size = sizes.get(j);
+							params[j].setParameter((i/combinationsSoFar)%size);
+							combinationsSoFar *= size;
+						}
+						results.add(function.evaluate());
+					}
+				}
+			});
 		}
+		runAndJoin(threads);
 		return results;
 	}
 
-	private static long combinations(List<Integer> sizes) {
-		long combinations = 1;
-		for (Integer size : sizes) {
-			combinations *= size;
+	private static void runAndJoin(List<Thread> threads) {
+		final List<Throwable> errors = Collections.synchronizedList(new ArrayList<Throwable>());
+		for (Thread thread: threads) {
+			thread.setUncaughtExceptionHandler(new Thread.UncaughtExceptionHandler() {
+				@Override
+				public void uncaughtException(Thread t, Throwable e) {
+					errors.add(e);
+				}
+			});
+			thread.start();
 		}
-		return combinations;
+		for (Thread thread: threads) {
+			try {
+				thread.join();
+			} catch (InterruptedException e) {
+				throw new RuntimeException(e);
+			}
+		}
+		for (Throwable throwable : errors) {
+			throw new RuntimeException(throwable);
+		}
+	}
+	
+	private static void sendMessageIfValid(Queue<Long> queue, long i, long combinations) {
+		if (i < combinations){
+			queue.add(i);
+		}
+	}
+
+	private static long combinations(List<Integer> sizes) {
+		BigInteger combinations = BigInteger.ONE;
+		for (Integer size : sizes) {
+			combinations = combinations.multiply(new BigInteger(size.toString()));
+		}
+		if (combinations.compareTo(new BigInteger(String.valueOf(Long.MAX_VALUE))) >=0) {
+			throw new UnsupportedOperationException("to many combinations: " + combinations);
+		}
+		return combinations.longValue();
 	}
 
 	@SuppressWarnings("rawtypes")
--- src/org/comprehend/Parameters.java
+++ src/org/comprehend/Parameters.java
@@ -1,6 +1,9 @@
 package org.comprehend;
 
 public class Parameters {
+	public static Parameter<Double> u = new Parameter<Double>();
+	public static Parameter<Double> v = new Parameter<Double>();
+	public static Parameter<Double> w = new Parameter<Double>();
 	public static Parameter<Double> x = new Parameter<Double>();
 	public static Parameter<Double> y = new Parameter<Double>();
 	public static Parameter<Double> z = new Parameter<Double>();
